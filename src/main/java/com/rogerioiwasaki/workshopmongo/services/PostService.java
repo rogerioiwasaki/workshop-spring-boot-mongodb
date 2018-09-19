84a114589fe458d20ecdd853631f9247335ac1bf
rogerioiwasaki <iwasaki.rogerio@gmail.com>
2018-09-19 15:43:28 -0300

    Obtendo post por id

---
+++ src/main/java/com/rogerioiwasaki/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.rogerioiwasaki.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rogerioiwasaki.workshopmongo.domain.Post;
+import com.rogerioiwasaki.workshopmongo.repository.PostRepository;
+import com.rogerioiwasaki.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
